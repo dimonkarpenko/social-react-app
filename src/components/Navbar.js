b6c0322de2b31a8187c10389604be02a39c7bad9
Dima Karpenko <134.2.zto@gmail.com>
2022-02-03 20:38:58 +0200

    master

---
+++ src/components/Navbar.js
@@ -0,0 +1,26 @@
+const Navbar = () => {
+    return (
+        <nav className='app-nav'>
+            <div>
+                <a href='#'>Prfole</a>
+            </div>
+            <div>
+                <a href='#'>
+                    Messages
+                </a>
+            </div>
+            <div>
+                <a href='#'>
+                    News
+                </a>
+            </div>
+            <div>
+                <a href='#'>
+                    Music
+                </a>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
